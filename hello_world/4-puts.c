c56c7cf0078617b2ae6ac045aa6808321ff918c5
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-21 11:58:33 +0000

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a specific string using puts
+ *
+ * Return: Always 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
