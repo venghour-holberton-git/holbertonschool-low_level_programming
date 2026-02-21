6fe082ba264266a3edcb2e2ae1e08fee82a9061d
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-21 21:39:16 +0000

    fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,10 +17,6 @@ int main(void)
 
 	last_digit = n % 10;
 
-	/* Fix negative last digit */
-	if (last_digit < 0)
-		last_digit = -last_digit;
-
 	printf("Last digit of %d is %d", n, last_digit);
 
 	if (last_digit > 5)
