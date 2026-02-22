38c750ff946392fd64fd84f9466584c39cdaee7f
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-22 22:19:41 +0000

    put char small char

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - doing main
+ * 
+ * description - print all char except q and e
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++){
+		if (c!= 'q' && c!= 'e'){
+			putchar(c);
+			putchar('\n');
+		}
+	}
+	return (0);
+}
