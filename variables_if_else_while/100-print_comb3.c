5192804a61d153bcee4bc0cb99f280d5a46ff9b2
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-23 00:59:03 +0000

    fix

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -13,15 +13,15 @@ int main(void)
 	for (a = 0; a <= 8; a++)
 	{
 		for (b = a + 1; b <= 9; b++)
+		{
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8 || b != 9)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-            			if (a != 8 || b != 9)
-	    			{
-                			putchar(',');
-                			putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
+		}
 	}
 	putchar('\n');
 
