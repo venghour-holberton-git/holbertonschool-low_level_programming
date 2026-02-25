8503f1a1ad6024c1cf395f3d5ac234d8d4ce6f80
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 09:35:28 +0000

    print number

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
