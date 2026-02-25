3822e9886915c77b74e1dff7379b7079ab8d67e3
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 07:48:16 +0000

    fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 	{
 		int i;
 
-		for (i = n; i <= 98; i--)
+		for (i = n; i >= 98; i--)
 		{
 			_putchar(i + '0');
 			_putchar(',');
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	{
 		int i;
 
-                for (i = n; i >= 98; i++)
+                for (i = n; i <= 98; i++)
                 {
                         _putchar(i + '0');
                         _putchar(',');
