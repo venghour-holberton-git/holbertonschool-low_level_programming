e7e4c5d6bb93b5d10d727562c0d4a425da3e20fc
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 11:30:46 +0000

    fix

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,20 +8,20 @@
 
 void print_diagonal(int n)
 {
-        int i, j;
+	int i, j;
 
 	if (n <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-        for (i = 0; i < n; i++)
-        {
-                for (j = 0; j < i; j++)
-                {
-                        _putchar(' ');
-                }
-                _putchar('\\');
-                _putchar('\n');
-        }
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+	_putchar('\n');
+	}
 }
