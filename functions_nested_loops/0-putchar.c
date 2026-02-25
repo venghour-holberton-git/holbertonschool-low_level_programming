aa8da18334cf6aad3d57d54ed3fb9491eb843915
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 02:58:54 +0000

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - entry point
- * description - print putchar
+ * main - prints _putchar followed by a new line using a loop
  * Return: 0
  */
-
 int main(void)
 {
-	printf("_putchar\n");
-	return (0);
+    char str[] = "_putchar";  // store the string in an array
+    int i = 0;
+
+    while (str[i] != '\0')    // loop until end of string
+    {
+        _putchar(str[i]);     // print one character
+        i++;
+    }
+
+    _putchar('\n');           // print newline
+    return 0;
 }
