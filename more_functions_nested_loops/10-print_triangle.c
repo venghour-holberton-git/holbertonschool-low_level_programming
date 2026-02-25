a39a6798736b92c5082035033a1703365229994c
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 13:48:57 +0000

    fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,8 @@
 
 void print_triangle(int size)
 {
-	int i,j;
+	int i, j;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
@@ -25,5 +26,4 @@ void print_triangle(int size)
 		}
 		_putchar('\n');
 	}
-}
-		
+}	
