62ffd5a3447384b8e85b07325f0817a4a3a66181
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-25 05:21:59 +0000

    print last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the number to check
+ *
+ * Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int last;
+
+	if (n < 0)
+		n = -n;
+
+	last = n % 10;
+	_putchar(last + '0');
+
+	return (last);
+}
