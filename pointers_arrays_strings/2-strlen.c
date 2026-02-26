0f55b11336d15d198db969f79c468b560bc4d606
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 03:49:38 +0000

    fix

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+int _strlen(char *s)
+{
+	return strlen(s);
+}
---
+++ pointers_arrays_strings/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char* n = "yyyt";
+	printf("__n__ %ld", strlen(n));
+}
