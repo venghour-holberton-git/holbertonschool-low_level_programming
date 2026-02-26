3eeb4dff8e2b8c76024a93621c4ca567748af0a6
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 04:32:19 +0000

    fix

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
+int _putchar(char c);
 int main(void)
 {
 	char* n = "Holberton!";
@@ -8,7 +10,13 @@ int main(void)
 	
 	while (n[i] != '\0')
 	{
+		_putchar(n[i]);
 		i++;
 	}
-	printf("i__ %d", i);
+	printf("\n");
+}
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
