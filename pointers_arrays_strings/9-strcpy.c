399e63633adfed5c6f7b59974006d593889a8784
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 11:31:30 +0000

    copy string

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string from src to dest
+ * @dest: buffer to copy to
+ * @src: string to copy from
+ *
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
