efa99e6073aad807ff503a2129462a69652843b8
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 10:03:23 +0000

    print every other character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character starting from the first character
+ * @str: input string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	while (str[i] == '\0')
+	{
+		if (i % 2 = 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+}
