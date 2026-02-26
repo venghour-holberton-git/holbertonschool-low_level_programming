ccb6ff709436b5dbe679af98e71b11a834b83397
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 10:18:25 +0000

    print n element

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: pointer to the first element of the array
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+	if (i != n - 1)
+		printf(",\t");
+	}
+
+	printf("\n");
+}
