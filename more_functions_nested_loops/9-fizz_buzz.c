b5edba6a7da1fe39d84a5f19a57efbae1aaa5714
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 00:29:00 +0000

    fix

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else
+		if (i % 3 != 0 && i % 5 != 0)
 		{
 			printf("%d", i);
 		}
-		printf(" ");
+		if (i != 100)
+		{
+			printf(" ");
+		}
 	}
+	printf("\n");
 	return (0);
 }
