a2742c910726236029af5f7e6a9a0412d8fa73e4
venghour-holberton-git <13067@holbertonstudents.com>
2026-02-26 10:13:08 +0000

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -6,21 +6,18 @@
  */
 void puts_half(char *str)
 {
-    int len = 0, i;
-    
-    /* find length of the string */
-    while (str[len] != '\0')
-        len++;
+	int len = 0, i;
 
-    /* compute starting index for second half */
-    i = (len % 2 == 0) ? len / 2 : (len + 1) / 2;
+	while (str[len] != '\0')
+	len++;
 
-    /* print from i to end */
-    while (str[i] != '\0')
-    {
-        _putchar(str[i]);
-        i++;
-    }
+	i = (len % 2 == 0) ? len / 2 : (len + 1) / 2;
 
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
