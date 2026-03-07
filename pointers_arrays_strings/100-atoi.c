d60dd98248b4f2772e7cc750ded9a41aed98e067
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-07 12:05:25 +0000

    fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -14,24 +14,30 @@
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int num = 0;
-	int started = 0;
+    int i = 0;
+    int sign = 1;
+    int num = 0;
+    int started = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == '-' && !started)
-			sign = -sign;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			started = 1;
-			num = num * 10 + (s[i] - '0');
-		}
-		else if (started)
-			break;
-		i++;
-	}
+    while (s[i])
+    {
+        if ((s[i] == '-' || s[i] == '+') && !started)
+        {
+            if (s[i] == '-')
+                sign = -sign;
+        }
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            started = 1;
+            if (sign == 1)
+                num = num * 10 + (s[i] - '0');
+            else
+                num = num * 10 - (s[i] - '0');
+        }
+        else if (started)
+            break;
 
-	return (num * sign);
+        i++;
+    }
+    return num;
 }
