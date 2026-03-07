5ca69f820f74b5e42ffb3801732ee5ab4dee6331
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-07 13:31:23 +0000

    fix

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strcmp - compare s1 and s2 base on the ASCII value of the characters in the string
+ * _strcmp - compare s1 and s2 base on the ASCII value of the 
+ * characters in the string
  * @s1: string 1
  * @s2: input string 2
  * Return: 0 if equal, negative if s1 < s2, positive if s2 < s1
@@ -15,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return s1[i] - s2[i];
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}
