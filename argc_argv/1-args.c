78b82aa49b801be3b61695b070faf27a75aaec26
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-08 10:56:07 +0000

    fix

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - prints the number of arguments passed to the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
 int main(int argc, char* argv[])
 {
 	(void) argv;
