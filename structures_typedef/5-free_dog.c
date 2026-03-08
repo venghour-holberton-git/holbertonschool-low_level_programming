55cb350ffd2495a09f66a8eff6d86d513cda841e
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-08 22:14:23 +0000

    free dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees a dog structure
+ * @d: pointer to the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
