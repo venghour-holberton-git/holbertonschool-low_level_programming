c515c9226a99529c6ea39b037c5c0c1fdbb1df26
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-08 14:02:56 +0000

    fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Stores basic info about a dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
