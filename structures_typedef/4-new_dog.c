ae475f757e7995f9e033ba0b98842cdba47a8e4f
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-08 14:55:14 +0000

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include "dog.h"
 
 /**
+ * _strdup_safe - allocates memory and copies a string
+ * @str: string to copy
+ *
+ * Return: pointer to new string, or NULL on failure
+ */
+char *_strdup_safe(char *str)
+{
+    char *copy;
+    unsigned int i, len = 0;
+
+    if (str == NULL)
+        return (NULL);
+
+    while (str[len] != '\0')
+        len++;
+
+    copy = malloc(len + 1);
+    if (copy == NULL)
+        return (NULL);
+
+    for (i = 0; i <= len; i++)
+        copy[i] = str[i];
+
+    return (copy);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
@@ -11,33 +38,28 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *d;
-	char *name_copy;
-	char *owner_copy;
-	unsigned int i, len_name = 0, len_owner = 0;
-	if (name == NULL || owner == NULL) return (NULL);
-	while (name[len_name] != '\0') len_name++;
-	while (owner[len_owner] != '\0') len_owner++;
-	d = malloc(sizeof(dog_t));
-	if (d == NULL) return (NULL);
-	name_copy = malloc(len_name + 1);
-	if (name_copy == NULL)
-	{
-		free(d);
-		return (NULL);
-	}
-	for (i = 0; i <= len_name; i++) name_copy[i] = name[i];
-	owner_copy = malloc(len_owner + 1);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
-		free(d);
-		return (NULL);
-	}
-	for (i = 0; i <= len_owner; i++)
-		owner_copy[i] = owner[i];
-	d->name = name_copy;
-	d->age = age;
-	d->owner = owner_copy;
-	return (d);
+    dog_t *d;
+
+    d = malloc(sizeof(dog_t));
+    if (d == NULL)
+        return (NULL);
+
+    d->name = _strdup_safe(name);
+    if (d->name == NULL)
+    {
+        free(d);
+        return (NULL);
+    }
+
+    d->owner = _strdup_safe(owner);
+    if (d->owner == NULL)
+    {
+        free(d->name);
+        free(d);
+        return (NULL);
+    }
+
+    d->age = age;
+
+    return (d);
 }
