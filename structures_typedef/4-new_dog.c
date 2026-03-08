3c9651c1bd759b5c50ff8551f18d0898ac2f5248
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-08 14:52:50 +0000

    fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,29 +15,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name_copy;
 	char *owner_copy;
 	unsigned int i, len_name = 0, len_owner = 0;
-
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	while (name[len_name] != '\0')
-		len_name++;
-	while (owner[len_owner] != '\0')
-		len_owner++;
-
+	if (name == NULL || owner == NULL) return (NULL);
+	while (name[len_name] != '\0') len_name++;
+	while (owner[len_owner] != '\0') len_owner++;
 	d = malloc(sizeof(dog_t));
-	if (d == NULL)
-		return (NULL);
-
+	if (d == NULL) return (NULL);
 	name_copy = malloc(len_name + 1);
 	if (name_copy == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-
-	for (i = 0; i <= len_name; i++)
-		name_copy[i] = name[i];
-
+	for (i = 0; i <= len_name; i++) name_copy[i] = name[i];
 	owner_copy = malloc(len_owner + 1);
 	if (owner_copy == NULL)
 	{
@@ -45,13 +34,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-
 	for (i = 0; i <= len_owner; i++)
 		owner_copy[i] = owner[i];
-	
 	d->name = name_copy;
 	d->age = age;
 	d->owner = owner_copy;
-
 	return (d);
 }
