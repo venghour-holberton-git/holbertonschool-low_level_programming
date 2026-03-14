01e4763be720fbd27c794bb25cc64db806977e3d
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-14 14:24:13 +0000

    fix

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,13 +3,18 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+ * print_number - print all number with seperator
+ * @seperator: input seperator
+ */
+
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i = 0;
 
 	va_start(args, n);
-	while(i < n)
+	while (i < n)
 	{
 		printf("%d", va_arg(args, int));
 		if (separator != NULL && i != (n - 1))
