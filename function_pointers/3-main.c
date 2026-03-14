b9e22ededc5eb1091da4a707b712f109fdb71116
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-14 10:52:15 +0000

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,25 +1,28 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+	int (*result)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+	if (!(strcmp(argv[2], "+") == 0 || strcmp(argv[2], "-") == 0 || strcmp(argv[2], "*") == 0 || strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && argv[3] == '0')
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && strcmp(argv[3], "0") == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	int (*result)(int, int) = get_op_func(argv[2]);
+	result = get_op_func(argv[2]);
 	printf("%d\n", result(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }
