82e4943787a50ef2a0e0d9cea41e275d9565dc69
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-14 10:58:33 +0000

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -12,12 +12,21 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (!(strcmp(argv[2], "+") == 0 || strcmp(argv[2], "-") == 0 || strcmp(argv[2], "*") == 0 || strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0))
+	if (!(
+		strcmp(argv[2], "+") == 0 ||
+		strcmp(argv[2], "-") == 0 ||
+		strcmp(argv[2], "*") == 0 ||
+		strcmp(argv[2], "/") == 0 ||
+		strcmp(argv[2], "%") == 0
+		))
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && strcmp(argv[3], "0") == 0)
+	if ((
+		strcmp(argv[2], "/") == 0 ||
+		strcmp(argv[2], "%") == 0) &&
+		strcmp(argv[3], "0") == 0)
 	{
 		printf("Error\n");
 		exit(100);
