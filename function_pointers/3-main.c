d1620706a3c845a222213b78850324b7cba2bbaf
venghour-holberton-git <13067@holbertonstudents.com>
2026-03-14 10:10:41 +0000

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,5 +9,10 @@ int main(int argc, char *argv[])
 		int (*result)(int, int) = get_op_func(argv[2]);
 		printf("%d\n", result(atoi(argv[1]), atoi(argv[3])));
 	}
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	return (0);
 }
