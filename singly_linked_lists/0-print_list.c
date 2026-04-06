534227de336c4341df0a613826aef981e183b3cc
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-06 14:12:18 +0000

    fix

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,7 +6,18 @@
 size_t print_list(const list_t *h)
 {
 	int i = 1;
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%u] %s\n", h->len, h->str);
+	}
 	if (h->next != NULL)
+	{
+		
 		i += print_list(h->next);
+	}
 	return (i);
 }
