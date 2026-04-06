e90955b692b886214d96c3a22b97e1545a91e87f
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-06 22:17:03 +0000

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,10 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new_node;
 	list_t *next_node = malloc(sizeof(list_t));
 	new_node = malloc(sizeof(list_t));
+	if (next_node != NULL || new_node != NULL)
+	{
+		return NULL;
+	}
 	next_node = *head;
 
 	while (str[len] != '\0')
