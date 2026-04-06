e4792c6a502477defc1f1b6e9656a33083eac679
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-06 15:33:18 +0000

    fix

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,7 +18,9 @@ list_t *add_node(list_t **head, const char *str)
 	{
 		len++;
 	}
-	*new_node = (list_t){strdup(str), len, next_node};
+	new_node->str = strdup(str);
+	new_node->len = len;
+	new_node->next = next_node;
 	*head = new_node;
 	return (new_node);
 }
