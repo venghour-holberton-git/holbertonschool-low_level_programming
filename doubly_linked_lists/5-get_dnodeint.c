b3ad26bd0d51501cfebef788030234621f17fb63
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 05:43:12 +0000

    get element at index

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: list head
+ * @index: index of element
+ * Return: the nth node of a dlistint_t linked list
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current_node;
+	int current_index = 0;
+
+	current_node = head;
+	while (current_node != NULL)
+	{
+		current_node = current_node->next;
+		if (current_index == index)
+			return (current_node);
+		current_index++;
+	}
+	return (NULL);
+}
