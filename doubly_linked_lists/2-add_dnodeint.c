ab7a2dafef026a631d43436ef01d16b0cc29b32c
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 02:59:46 +0000

    add new node to the beginnning

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint -  add new node to the begining
+ * @head: list head
+ * @n: insert integer
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current_node;
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+	current_node = *head;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = current_node;
+	*head = new_node;
+	return (*head);
+}
