67b3a35bdd73cc98c3202d7a5fa06b91e55c9494
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 02:18:29 +0000

    return number of elements

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return number of elements in the link
+ * @h: link
+ * Return: number of element in the link
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 1;
+	if (h == NULL)
+		return (0);
+	count += dlistint_len(h->next);
+	return (count);
+}
