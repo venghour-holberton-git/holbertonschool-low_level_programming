34298e68fc4547c92b6bdcc2237b543fdad2156a
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 07:09:55 +0000

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *current_node;
 
 	current_node = *h;
-	while (current_node->next != NULL)
+	while (current_node != NULL)
 	{
 		if (current_index == idx)
 		{
@@ -23,8 +23,15 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 				return (NULL);
 			new_node->n = n;
 			new_node->next = current_node;
-			new_node->prev = current_node->prev;
-			current_node->prev->next = new_node;
+			if (current_node->prev != NULL)
+			{
+				current_node->prev->next = new_node;
+				new_node->prev = current_node->prev;
+			}
+			else
+			{
+				new_node->prev = NULL;
+			}
 			current_node->prev = new_node;
 			return (new_node);
 		}
