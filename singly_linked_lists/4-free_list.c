072bcdb1b614c23dbf00b7777028463aeec77860
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 00:52:22 +0000

    free list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/** free_list - free the list
+ * @head: list head
+ */
+
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
+}
