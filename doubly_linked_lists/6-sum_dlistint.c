fe7b3b6de1aac44fbe4f9673023913b8e57afe92
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-07 06:13:23 +0000

    return sum function

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - sum and return all int in the list
+ * @head: list to be sum
+ * Return: the sum result
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return 0;
+	return head->n + sum_dlistint(head->next);
+}
