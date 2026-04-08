3c94225e25c8dee4d59b373c71d4c7d25c16b62f
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 09:47:01 +0000

    fix

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
-		return (-1);
+		return (1);
 	while (text_content[len] != '\0')
 	{
 		len++;
