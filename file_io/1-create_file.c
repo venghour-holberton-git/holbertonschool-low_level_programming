f4f2990f70699d62d6333c8bbe386b3ea6ac73da
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 09:04:41 +0000

    fix

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,10 +13,10 @@ int create_file(const char *filename, char *text_content)
 	size_t len = 0;
 	
 	if (filename == NULL)
-		return (0);
+		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[len] != '\0')
