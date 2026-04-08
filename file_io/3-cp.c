80e60dc8027496c63162773a941f30faa63e43c2
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 11:20:20 +0000

    fix

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -8,7 +8,10 @@ int main(int argc, char **argv)
 	char buffer[1024];
 
 	if (argc < 3)
+	{
+		dprint(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
+	}
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
