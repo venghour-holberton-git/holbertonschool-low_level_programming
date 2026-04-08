81a1b7d173082ed1d63e2ed074472e552c25342f
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 10:54:30 +0000

    copy from text form one file to another

---
+++ file_io/3-cp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+	int fd;
+	int fd2;
+	ssize_t read_bytes;
+	char buffer[1024];
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+		return (0);
+	read_bytes = read(fd, buffer, sizeof(buffer));
+	fd2 = open(argv[2], O_CREAT | O_WRONLY, 0666);
+	if (fd2 == -1)
+		return (0);
+	write(fd2, buffer, read_bytes);
+	return (0);
+}
