a1401474c6383b47f83a4f9e47e4604d2439b2dc
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 09:06:02 +0000

    fix betty

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	size_t len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
