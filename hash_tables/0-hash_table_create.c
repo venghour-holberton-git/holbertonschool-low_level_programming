ddc12eaed5ef2b3bee045f74b32a7d498d9e5879
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 14:37:38 +0000

    create a hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = BUCKET_SIZE;
+	hash_table->array = malloc(sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+		return (NULL);
+	return (hash_table);
+}
