6f08a13804828cbaaa14666ec097874da99f627c
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-08 08:40:00 +0000

    create file function

---
+++ file_io/1-create_file.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_file - create file
+ * @filename: filename
+ * @text_content: content inside the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	char *filepath;
+	size_t len;
+
+	filepath = malloc(strlen("./") + strlen(filename));
+	strcpy(filepath, "./");
+	strcat(filepath, filename);
+	
+	fd = creat(filepath, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		while (text_content[len] != '\0')
+		{
+			len++;
+		}
+		write(fd, text_content, len);
+	}
+	return (len);
+}
