99a7c03d40702da8a587b9723142dec3f87a016b
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-09 18:51:23 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_node;
 	hash_node_t *current_node;
-	hash_node_t *prev_node;
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
