4f6ceb8cd18ff8488fce045250f47ce6940692c3
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-09 18:41:55 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,7 +10,7 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, ht->size);
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *new_node;
 	hash_node_t *current_node;
 
