8fa3251fd49b143a052ae23b1844373f8212376b
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-19 22:37:17 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	hash_node_t *current_node;
 
-	if (ht == NULL || key == NULL || *key = '\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 	{
 		index = key_index((const unsigned char *)key, ht->size);
 		return (0);
