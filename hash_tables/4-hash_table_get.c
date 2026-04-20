75c2cb7fbaf35583c3ffc9586a05f3633afa97a9
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 00:42:53 +0000

    implement the get function

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,7 +2,26 @@
 
 /**
  * hash_tale_get - get value with associated key from hash table
+ * @ht: hash table to retrieve value
+ * @key: key to search for value
+ * Return: value store
+ */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *current_node;
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	
+	return (NULL);
 }
