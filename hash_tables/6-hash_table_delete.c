1aae717c57bad690839ecc35d14eaa4a87fda1cb
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 02:30:16 +0000

    delete hash table

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+    unsigned long int index;
+    hash_node_t *node;
+    hash_node_t *tmp;
+
+    if (ht == NULL)
+        return;
+
+    for (index = 0; index < ht->size; index++)
+    {
+        node = ht->array[index];
+
+        while (node != NULL)
+        {
+            tmp = node->next;
+
+            free(node->key);
+            free(node->value);
+            free(node);
+
+            node = tmp;
+        }
+    }
+
+    free(ht->array);
+    free(ht);
+}
