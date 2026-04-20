68e99889c48282cec687130a2f1062463e562c90
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 02:38:00 +0000

    fix

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -15,39 +15,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *current_node;
 
 	if (ht == NULL || key == NULL || *key == '\0')
-	{
-		return (0);
-	}
+	return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-	{
-		return (0);
-	}
-	new_node->next = NULL;
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-		return (1);
-	}
 	current_node = ht->array[index];
+
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
+			free(current_node->value);
 			current_node->value = strdup(value);
 			return (1);
 		}
-		current_node = current_node->next;
+	current_node = current_node->next;
 	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	}
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
