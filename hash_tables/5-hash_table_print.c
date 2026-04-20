0ee56d7f2bcf1d60728b89c25d501f65faf2e8b5
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 02:12:12 +0000

    print all hash element

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/** hash_table_print - print hash table
+ * @ht: hash_table to be print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *current_node;
+	int index = 0;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	while (index < ht->size)
+	{
+		if (ht->array[index] != NULL)
+		{
+			current_node = ht->array[index];
+			while (current_node != NULL)
+			{
+				if (!first)
+					printf(", ");
+				first = 0;
+				printf("%s : %s", current_node->key, current_node->value);
+				current_node = current_node->next;
+			}
+		}
+		index++;
+	}
+	printf("}\n");
+}
+
