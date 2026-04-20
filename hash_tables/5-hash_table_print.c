dd026700c126b39a531777b016e39dfae01462c1
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 02:16:47 +0000

    fix

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -23,7 +23,7 @@ void hash_table_print(const hash_table_t *ht)
 				if (!first)
 					printf(", ");
 				first = 0;
-				printf("'%s' : '%s'", current_node->key, current_node->value);
+				printf("'%s': '%s'", current_node->key, current_node->value);
 				current_node = current_node->next;
 			}
 		}
